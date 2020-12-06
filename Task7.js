75dca7ab621c265919ee580f82e273b333a97722
Lindokuhle Shabalala <mlixshaba@gmail.com>
2020-12-06 23:25:30 +0000

    finished Task 7

---
+++ Task7.js
@@ -0,0 +1,9 @@
+//from celsius to fahrenheit
+function getFahrenhetTemperature (celsius) {
+   return `${(celsius * (9/5)) + 32} F`
+}
+
+//from fahrenheit to celsius
+function getCelsiusTemperature (fahrenheit) {
+    return `${(fahrenheit -32) * (5/9)} C`
+ }
