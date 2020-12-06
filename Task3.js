c6ae427f017b74517bdb30cae28d1701378fd837
Lindokuhle Shabalala <mlixshaba@gmail.com>
2020-12-06 20:20:01 +0000

    finished task 3

---
+++ Task3.js
@@ -0,0 +1,12 @@
+function checkNumbers (num1,num2){
+    if (num1 == 65){
+        return true
+    }
+    else if (num2 == 65){
+        return true
+    }
+    else if ( num1 + num2 == 65){
+        return true
+    }
+    else return false
+}
